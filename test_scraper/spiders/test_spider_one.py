5a82a6c5e06f72445677d5fab9565c0d312e236d
NLSanyu <lydiansanyu@gmail.com>
2020-09-17 23:13:39 +0300

    Add linting

--- test_scraper/spiders/test_spider_one.py
+++ test_scraper/spiders/test_spider_one.py
@@ -1,19 +1,26 @@
+import os
 import scrapy
-from decouple import config
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class TestSpiderOne(scrapy.Spider):
     name = "test_one"
     start_urls = [
-        config['SITE_URL_1']
+        os.environ.get("site_url_1")
     ]
-    
+
     def parse(self, response):
         for property_item in response.css('div.property-card'):
             yield {
-                external_link: property_item.css('a.property-card__link::attr(href)').get(),
-                title: property_item.css('h4.property-card__title::text').get(),
-                square_meters: property_item.css('span.property-card__feature::text')[0].get(),
-                room_count: property_item.css('span.property-card__feature::text')[1].get(),
-                rent: property_item.css('span.property-card__price::text').get()
+                'external_link':
+                    property_item.css('a.property-card__link::attr(href)').get(),
+                'title':
+                    property_item.css('h4.property-card__title::text').get(),
+                'square_meters':
+                    property_item.css('span.property-card__feature::text')[0].get(),
+                'room_count':
+                    property_item.css('span.property-card__feature::text')[1].get(),
+                'rent':
+                    property_item.css('span.property-card__price::text').get()
             }
-
--- test_scraper/spiders/test_spider_two.py
+++ test_scraper/spiders/test_spider_two.py
@@ -1,14 +1,18 @@
+import os
 import scrapy
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class TestSpiderTwo(scrapy.Spider):
     name = "test_two"
     start_urls = [
-        config['SITE_URL_2']
+        os.environ.get("site_url_2")
     ]
-    
+
     def parse(self, response):
         for property_item in response.css('div.listing-houses'):
             yield {
-                external_link: property_item.css('a.houses-url::attr(href)').get(),
+                'external_link':
+                    property_item.css('a.houses-url::attr(href)').get(),
             }
